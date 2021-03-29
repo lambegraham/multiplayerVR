b5885d439b52fbf7464b89e0589d1c59dddd676d
lambegraham <lambegraham@gmail.com>
2021-03-29 21:37:28 +0100

    Added HandSync for left

--- multiplayerVRUnity/Assets/myAssets/Scripts/HandSync.cs
+++ multiplayerVRUnity/Assets/myAssets/Scripts/HandSync.cs
@@ -36,6 +36,24 @@ public class HandSync : MonoBehaviour, IPunObservable
     private Quaternion NetworkRotation_RightHand;
     private float Angle_RightHand;
 
+    bool firstTake = false;
+
+    private void OnEnable()
+    {
+        firstTake = true;
+    }
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        StoredPos_LeftHand = leftHand.localPosition;
+        NetworkPos_LeftHand = Vector3.zero;
+        NetworkRotation_LeftHand = Quaternion.identity;
+
+        StoredPos_RightHand = rightHand.localPosition;
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -45,18 +63,64 @@ public class HandSync : MonoBehaviour, IPunObservable
     // Update is called once per frame
     void Update()
     {
-        
+        if (!photonView.IsMine) 
+        {
+            leftHand.localPosition = Vector3.MoveTowards(leftHand.localPosition, NetworkPos_LeftHand, Distance_LeftHand * (1.0f / PhotonNetwork.SerializationRate));
+            leftHand.localRotation = Quaternion.RotateTowards(leftHand.localRotation, NetworkRotation_LeftHand, Angle_LeftHand * (1.0f / PhotonNetwork.SerializationRate));
+        }
+
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
+            //Send left hand pos data
+
+            Direction_LeftHand = leftHand.localPosition - StoredPos_LeftHand;
+            StoredPos_LeftHand = leftHand.transform.localPosition;
+
+            stream.SendNext(leftHand.localPosition);
+            stream.SendNext(Direction_LeftHand);
+
+            //Send left hand rotation data 
+            stream.SendNext(leftHand.localRotation);
 
         }
         else
         {
+            //Recieve left hand position data
+            NetworkPos_LeftHand = (Vector3)stream.ReceiveNext();
+            Direction_LeftHand = (Vector3)stream.ReceiveNext();
 
+            if (firstTake)
+            {
+                leftHand.localPosition = NetworkPos_LeftHand;
+                Distance_LeftHand = 0;
+            }
+
+            else
+            {
+                float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+                //PhotonNetwork.Time syncs time for all players
+                NetworkPos_LeftHand += Direction_LeftHand * lag;
+                Distance_LeftHand = Vector3.Distance(leftHand.localPosition, NetworkPos_LeftHand);
+            }
+
+            NetworkRotation_LeftHand = (Quaternion)stream.ReceiveNext();
+            if (firstTake)
+            {
+                Angle_LeftHand = 0;
+                leftHand.localRotation = NetworkRotation_LeftHand;
+            }
+            else
+            {
+                Angle_LeftHand = Quaternion.Angle(leftHand.localRotation, NetworkRotation_LeftHand);
+            }
+            if (firstTake)
+            {
+                firstTake = false;
+            }
         }
     }
 }
